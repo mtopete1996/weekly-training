7e6810977609549380e576998ac837fea36339b6
Manuel Topete <mtopete.dev@gmail.com>
2024-05-21 13:26:46 -0600

    Making index view work with tailwind

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
-'use strict';
+"use strict";
 
-const Hapi = require('@hapi/hapi');
+const Hapi = require("@hapi/hapi");
+const Path = require("path");
 
 const init = async () => {
   const server = Hapi.server({
-      port: 3000,
-      host: 'localhost'
+    port: 3000,
+    host: "localhost",
+    routes: {
+      files: {
+        relativeTo: Path.join(__dirname, "assets"),
+      },
+    },
+  });
+
+  await server.register(require("@hapi/vision"));
+  await server.register(require('@hapi/inert'));
+
+  server.route({
+    method: "GET",
+    path: "/assets/{param*}",
+    handler: function (req, h) {
+      return h.file(Path.join(__dirname, "assets", req.params.param));
+    },
+  });
+
+  server.views({
+    engines: {
+      html: require("handlebars"),
+    },
+    relativeTo: __dirname,
+    path: "templates",
+    layout: true,
+    layoutPath: "templates/layouts",
+  });
+
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: function (_req, h) {
+      return h.view("index", { title: 'hello!'});
+    }
   });
 
   await server.start();
-  console.log('Server running on %s', server.info.uri);
-
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-        return 'Hello World!';
-    }
-  });
 };
 
-process.on('unhandledRejection', (err) => {
+process.on("unhandledRejection", (err) => {
   console.log(err);
   process.exit(1);
 });
